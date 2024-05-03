4807315b22b410d73476715ce89d6d0901950053
suong1272000 <suong1272000@gmail.com>
2024-05-03 22:22:42 +0900

    feat: overflow-y-auto 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <header>
           <NavBar />
         </header>
-        <section className="bg-black h-full overflow-y-auto ">
+        <section className="bg-black h-full " style={{ marginTop: '68px' }}>
           {children}
         </section>
         <footer>
